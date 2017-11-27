a55155f82925f8696e4b83e67a00a2661321927d
mouned <moussa.nedjari92@gmail.com>
2017-11-27 09:44:58 +0100

    init maven spring project

---
+++ src/main/java/fr/dta/projectDTA/ProjectDtaApplication.java
@@ -0,0 +1,12 @@
+package fr.dta.projectDTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDtaApplication.class, args);
+	}
+}
---
+++ src/main/java/fr/dta/projectDTA/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.dta.projectDTA;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectDtaApplication.class);
+	}
+
+}
